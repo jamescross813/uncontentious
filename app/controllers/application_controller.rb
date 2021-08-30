430fb24adb02d292b60ef5aefc7715c9267c2b0b
jamescross <crossjames813@gmail.com>
2021-08-30 12:03:29 -0500

    removed middleware

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
 
-    config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins '*'
-          resource '*',
-            :headers => :any,
-            :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-            :max_age => 0
-        end
-      end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #     allow do
+    #       origins '*'
+    #       resource '*',
+    #         :headers => :any,
+    #         :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+    #         :max_age => 0
+    #     end
+    #   end
 end
