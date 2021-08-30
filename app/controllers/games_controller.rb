df25e0fa13804ffcddec4092be6de14dc4002231
jamescross <crossjames813@gmail.com>
2021-08-30 12:03:08 -0500

    controller action games show index added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,5 +5,12 @@ class GamesController < ApplicationController
         render json: games
     end
     
- 
+    def show
+        game = Game.find_by(id: params[:id])
+        if game 
+            render json: game
+        else
+            render json: {message: "Game not found, please try again or add the game to the records"}
+        end
+    end
 end
