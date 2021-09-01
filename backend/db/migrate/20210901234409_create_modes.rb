5228db39242db4509c43c15619f702c75644abb3
jamescross <crossjames813@gmail.com>
2021-09-01 18:51:57 -0500

    mode table complete

--- backend/db/migrate/20210901234409_create_modes.rb
+++ backend/db/migrate/20210901234409_create_modes.rb
@@ -2,7 +2,7 @@ class CreateModes < ActiveRecord::Migration[6.1]
   def change
     create_table :modes do |t|
       t.string :mode_name
-      t.references :game, null: false, foreign_key: true
+      
 
       t.timestamps
     end
