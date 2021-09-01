fee200dffbae7b4c8f0e10a455ee93a0aafc878f
jamescross <crossjames813@gmail.com>
2021-08-31 19:20:08 -0500

    practice to check link up

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+const greet = document.getElementById("greeting")
+
+document.addEventListener("DOMContentLoaded", () => {
+ function greeting(){
+        return console.log(greet)
+    }
+
+    greeting()
+    })
+    
+   
