2ddf878adbcb2d19009aab88aa1515ed2b7d279d
jamescross <crossjames813@gmail.com>
2021-08-31 20:03:35 -0500

    searchfor button constant fixed

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,14 @@
-const searchForButton = document.getElementById("add")
+const searchForButton = document.getElementById("search")
+const addButton = document.getElementById("add")
 
 document.addEventListener("DOMContentLoaded", function() {
    
-  
+    searchForButton.addEventListener('click', function(){
+        
+    })
+
+    addButton.addEventListener('click', function(){
+        
+    })
 
 }); 
