ca5095aee455b19a94c44d68bcc08851da52424a
jamescross <crossjames813@gmail.com>
2021-09-02 12:12:22 -0500

    basic fetchGames method

--- frontend/render.js
+++ frontend/render.js
@@ -6,9 +6,22 @@ const addForm = document.getElementById("add-game-form");
 function fetchGames() {
     fetch(`${basicUrl}/games`)
     .then(resp =>resp.json())
-    .then(data=>console.log(data))
+    .then(data =>renderGames(data))
   };
+
+function renderGames(data){
+    const game_card = document.createElement('div');
+    document.body.appendChild(game_card)
+    game_card.elementId = 'game-card';
+
+    data.map(r => {
+        let gameTitle = document.createElement('h3')
+        gameTitle.innerHTML = r.title
+        game_card.append(gameTitle)
+    })
+}
   
 document.addEventListener("DOMContentLoaded", function() {
     fetchGames()
+    renderGames()
 })
