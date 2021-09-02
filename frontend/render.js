babf15a34fef9aad7b732bf887e49146fdac277a
jamescross <crossjames813@gmail.com>
2021-09-02 17:47:40 -0500

    render individual game function

--- frontend/render.js
+++ frontend/render.js
@@ -4,35 +4,32 @@ const searchForm = document.getElementById("search-games");
 const addForm = document.getElementById("add-game-form");
 const gameCard = document.getElementById('game-card')
 
-function fetchGames() {
-    fetch(`${basicUrl}/games`)
-    .then(resp =>resp.json())
-    .then(data =>renderGames(data))
-  };
-
 function renderGames(data){
     data.map(r => {
-        let gameTitle = document.createElement('h3')
-        gameTitle.innerHTML = r.game_title
-                
-        let gameDescription = document.createElement('p')
-        gameDescription.innerHTML = r.description
-
-        let gamePlayers = document.createElement('li')
-        gamePlayers.innerHTML = `Number of players: ${r.min_players} - ${r.max_players}`
-
-        let gamePlayStyle = document.createElement('li')
-        gamePlayStyle.innerHTML = `Play Style: ${r.play_style}`
-
-        let gameTime = document.createElement('li')
-        gameTime.innerHTML = `Max Time: ${r.max_time}`
-
-        let gameType = document.createElement('li')
-        gameType.innerHTML = `Game Type: ${r.game_type}`
-
-        gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType)
-
+        renderGame(r)
     })
+}
+
+function renderGame(data){
+    let gameTitle = document.createElement('h3')
+    gameTitle.innerHTML = data.game_title
+                
+    let gameDescription = document.createElement('p')
+    gameDescription.innerHTML = data.description
+
+    let gamePlayers = document.createElement('li')
+    gamePlayers.innerHTML = `Number of players: ${data.min_players} - ${data.max_players}`
+
+    let gamePlayStyle = document.createElement('li')
+    gamePlayStyle.innerHTML = `Play Style: ${data.play_style}`
+
+    let gameTime = document.createElement('li')
+    gameTime.innerHTML = `Max Time: ${data.max_time}`
+
+    let gameType = document.createElement('li')
+    gameType.innerHTML = `Game Type: ${data.game_type}`
+
+    gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType)
 }
 
 function searchGames(data, input){
