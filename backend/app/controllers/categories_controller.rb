b74e23e7df70959a3ccf47b54a80151f41186f8b
jamescross <crossjames813@gmail.com>
2021-09-02 09:32:50 -0500

    category index and show actions added

--- backend/app/controllers/categories_controller.rb
+++ backend/app/controllers/categories_controller.rb
@@ -1,2 +1,17 @@
 class CategoriesController < ApplicationController
+    def index
+        categories = Category.all
+        render json: categories, 
+            except: [:created_at, :updated_at]
+    end
+    
+    def show
+        category = Category.find_by(id: params[:id])
+        if category 
+            render json: category,
+            except: [:created_at, :updated_at]
+        else
+            render json: {message: "Category not found"}
+        end
+    end
 end
