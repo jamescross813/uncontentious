237a50080e956756bda751b3940136081530f15f
jamescross <crossjames813@gmail.com>
2021-09-02 12:29:43 -0500

    game card render function basic

--- frontend/render.js
+++ frontend/render.js
@@ -10,14 +10,31 @@ function fetchGames() {
   };
 
 function renderGames(data){
-    const game_card = document.createElement('div');
-    document.body.appendChild(game_card)
-    game_card.elementId = 'game-card';
+    const gameCard = document.createElement('div');
+    document.body.appendChild(gameCard)
+    gameCard.elementId = 'game-card';
 
     data.map(r => {
         let gameTitle = document.createElement('h3')
         gameTitle.innerHTML = r.title
-        game_card.append(gameTitle)
+        
+        let gameDescription = document.createElement('p')
+        gameDescription.innerHTML = r.description
+
+        let gamePlayers = document.createElement('li')
+        gamePlayers.innerHTML = `Number of players: ${r.min_players} - ${r.max_players}`
+
+        let gamePlayStyle = document.createElement('li')
+        gamePlayStyle.innerHTML = `Play Style: ${r.play_style}`
+
+        let gameTime = document.createElement('li')
+        gameTime.innerHTML = `Max Time: ${r.max_time}`
+
+        let gameType = document.createElement('li')
+        gameType.innerHTML = `Game Type: ${r.game_type}`
+
+        gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType)
+
     })
 }
   
