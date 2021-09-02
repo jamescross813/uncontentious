490bb5b1e9d156eeb2a88813f0be7299d7c2de2c
jamescross <crossjames813@gmail.com>
2021-09-02 10:27:29 -0500

    constants added

--- frontend/render.js
+++ frontend/render.js
@@ -1,1 +1,4 @@
 const basicUrl= "http://localhost:3000/books"
+const gameList = document.getElementById('game-list');
+const searchForm = document.getElementById("search-games");
+    const addForm = document.getElementById("add-game-form");
