7f91f2dcbcbd514259eef3d470e528ddb8c41166
jamescross <crossjames813@gmail.com>
2021-09-02 11:55:26 -0500

    fetch games function

--- frontend/render.js
+++ frontend/render.js
@@ -1,4 +1,16 @@
-const basicUrl= "http://localhost:3000/books"
+const basicUrl= "http://localhost:3000/"
 const gameList = document.getElementById('game-list');
 const searchForm = document.getElementById("search-games");
 const addForm = document.getElementById("add-game-form");
+
+function fetchGames() {
+    fetch(`${basicUrl}/games`)
+    .then(resp =>resp.json())
+    .then(data=>console.log(data))
+    
+    
+  };
+  
+document.addEventListener("DOMContentLoaded", function() {
+    fetchGames()
+})
