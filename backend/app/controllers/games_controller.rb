4668b28348643374bac64b9a1cd8278b1bbe2556
jamescross <crossjames813@gmail.com>
2021-09-02 08:36:34 -0500

    except code added to streamline show rednder

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -10,7 +10,8 @@ class GamesController < ApplicationController
     def show
         game = Game.find_by(id: params[:id])
         if game 
-            render json: game
+            render json: game,
+            except: [:created_at, :updated_at]
         else
             render json: {message: "Game not found, please try again or add the game to the records"}
         end
