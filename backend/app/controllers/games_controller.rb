507e659a7389f1d492ec23def5f0ecd2a6ee0a35
jamescross <crossjames813@gmail.com>
2021-09-02 08:28:39 -0500

    cors middleware code added

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,5 +1,20 @@
 class GamesController < ApplicationController
 
+    def new
+        @game = Game.new(user_id: params[:user_id])
+    end
+
+    def create 
+        @game = Game.new(game_params)
+        
+        if @game.valid?
+           @game.save
+           redirect_to user_game_path(@game.user_id, @game)
+        else 
+            render :new
+        end
+    end
+
     def index
         games = Game.all
         render json: games
@@ -13,4 +28,40 @@ class GamesController < ApplicationController
             render json: {message: "Game not found, please try again or add the game to the records"}
         end
     end
+
+    def edit
+        if game_check
+            @game = Game.find(params[:id])
+            render :edit
+        else
+            render :'application/failure'
+        end
+    end
+
+    def update
+        @game = Game.find(params[:id])
+        @game.update(game_params)
+        redirect_to user_game_path(@game.user_id, @game)
+    end
+
+    def destroy
+        if game_check
+            @game = Game.find(params[:id])
+            @game.destroy
+            redirect_to user_path(session[:user_id])
+        else 
+            render :'application/failure'
+        end
+    end
+
+
+    private
+
+    def game_params
+        params.require(:game).permit(:title, :order, :user_id, :scenario_ids => [], :scenarios_attributes=> [:title,
+                                                                                                            :order_number,
+                                                                                                            :completion_status,
+                                                                                                            :run_number])
+    end
+
 end
--- backend/config/application.rb
+++ backend/config/application.rb
@@ -37,4 +37,14 @@ module Uncontentious
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+        :max_age => 0
+    end
+  end
 end
