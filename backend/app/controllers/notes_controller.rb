74bcbf76117f95106b59b999466f5b95214b441d
jamescross <crossjames813@gmail.com>
2021-09-02 09:36:21 -0500

    index and show actions added to controller

--- backend/app/controllers/notes_controller.rb
+++ backend/app/controllers/notes_controller.rb
@@ -1,2 +1,20 @@
 class NotesController < ApplicationController
+
+    def index
+        notes = Note.all
+        render json: notes, 
+            except: [:created_at, :updated_at]
+    end
+    
+    def show
+        notes = Note.find_by(id: params[:id])
+        if note
+            render json: notes,
+            except: [:created_at, :updated_at]
+        else
+            render json: {message: "Note not found"}
+        end
+    end
+
+    
 end
