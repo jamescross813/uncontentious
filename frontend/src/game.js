9056bd6176860310568c9c5b57b09bf249a81bfe
jamescross <crossjames813@gmail.com>
2021-09-04 20:05:05 -0500

    new form code moced to game class

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -62,4 +62,37 @@ static all = []
     
         gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType)
     }
+
+    async newGamePost(){
+
+        let inputValue = document.getElementsByClassName("input-text");
+        let titleInput = document.getElementById('title-input');
+    
+        let gameFormData = {
+            description: inputValue[3].value,
+            min_players: inputValue[1].value,
+            max_players: inputValue[2].value,
+            max_time: inputValue[7].value,
+            play_style: inputValue[6].value,
+            game_type: inputValue[4].value,
+            game_title: titleInput.value
+            };
+    
+        let configObj = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(gameFormData)
+        };
+    
+        fetch(`${basicUrl}/games`, configObj)
+        .then(resp => resp.json())
+        .then(data => {
+            fetchGame(data.id);
+            
+        })
+    
+    }
 }
