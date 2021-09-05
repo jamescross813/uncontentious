6a5dbf49ae4b3f48fb420bb6ad1e0454885e8397
jamescross <crossjames813@gmail.com>
2021-09-04 21:11:46 -0500

    uncommented unused code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,61 +1,53 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    const searchFormButton = document.getElementById("search-for");
-    const addFormButton = document.getElementById("add");
-    const searchForm = document.getElementById("search-games");
-    const addForm = document.getElementById("add-game-form");
-    const viewButton = document.getElementById("games-list");
-    const searchButton = document.getElementById("submit-search")
-    const addSubmit = document.getElementById("add-submit")
-    const titleOne = document.querySelector('h3')
-    const randomize = document.getElementById("random-game")
     
-    searchFormButton.addEventListener('click', ()=> {
-        if(!addForm.hidden){
-            addForm.hidden = !addForm.hidden 
-           console.log("done")
-        } 
-        searchForm.hidden = !searchForm.hidden
-    });
+    
+    // searchFormButton.addEventListener('click', ()=> {
+    //     if(!addForm.hidden){
+    //         addForm.hidden = !addForm.hidden 
+    //        console.log("done")
+    //     } 
+    //     searchForm.hidden = !searchForm.hidden
+    // });
 
-    addFormButton.addEventListener('click', function () {
-        if(!searchForm.hidden){
-            searchForm.hidden = !searchForm.hidden 
-        } 
-        addForm.hidden = !addForm.hidden
+    // addFormButton.addEventListener('click', function () {
+    //     if(!searchForm.hidden){
+    //         searchForm.hidden = !searchForm.hidden 
+    //     } 
+    //     addForm.hidden = !addForm.hidden
         
-    })
+    // })
 
     viewButton.addEventListener('click', () =>{
         console.log("clicked")
     })
 
-    searchButton.addEventListener('click', function(){
-        searchGames()
-    })
+    // searchButton.addEventListener('click', function(){
+    //     searchGames()
+    // })
 
-    addSubmit.addEventListener('click', function(){
-        newGamePost()
-    })
+    // addSubmit.addEventListener('click', function(){
+    //     newGamePost()
+    // })
 
-    titleOne.addEventListener('mouseover', function(){
-        console.log("thing is happening")
-    })
+    // titleOne.addEventListener('mouseover', function(){
+    //     console.log("thing is happening")
+    // })
 
-    randomize.addEventListener('click', () =>{
-        let randomNum
-        function randomNumGen(){
-            return randomNum = Math.floor(Math.random()* 10)  
-        }
-       console.log(randomNum)
-        if(fetchGame(randomNum)===true){
-            renderGame(randomNum)
-        }else{
-            randomNumGen()
-        }
+    // randomize.addEventListener('click', () =>{
+    //     let randomNum
+    //     function randomNumGen(){
+    //         return randomNum = Math.floor(Math.random()* 10)  
+    //     }
+    //    console.log(randomNum)
+    //     if(fetchGame(randomNum)===true){
+    //         renderGame(randomNum)
+    //     }else{
+    //         randomNumGen()
+    //     }
         
 
-    })
+    // })
     
 
 }); 
