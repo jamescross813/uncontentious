ec622273a06ad6d34592847e457ca583a9624479
jamescross <crossjames813@gmail.com>
2021-09-04 20:01:35 -0500

    added render methods

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -30,4 +30,36 @@ static all = []
             }
         })
     };
+
+    renderGames(data){   
+        data.map(r => {
+            let gameName 
+            gameName.innerHTML = `<option value="${r.gameTitle}">${r.gameTitle}</option>`
+            
+        })
+    }
+    
+    renderGame(data){
+        gamesCard.hidden
+        gameTitle = document.createElement('a')
+        gameTitle.innerHTML = data.game_title
+        gameTitle.id = "titles"
+        gameCard.hidden = false            
+        let gameDescription = document.createElement('p')
+        gameDescription.innerHTML = data.description
+    
+        let gamePlayers = document.createElement('li')
+        gamePlayers.innerHTML = `Number of players: ${data.min_players} - ${data.max_players}`
+    
+        let gamePlayStyle = document.createElement('li')
+        gamePlayStyle.innerHTML = `Play Style: ${data.play_style}`
+    
+        let gameTime = document.createElement('li')
+        gameTime.innerHTML = `Max Time: ${data.max_time}`
+    
+        let gameType = document.createElement('li')
+        gameType.innerHTML = `Game Type: ${data.game_type}`
+    
+        gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType)
+    }
 }
