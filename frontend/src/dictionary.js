335cd9dc60a0fadc7d8a024ba9ec1e5b2964f6c9
jamescross <crossjames813@gmail.com>
2021-09-04 21:14:08 -0500

    updated domcontentlisentner

--- frontend/src/dictionary.js
+++ frontend/src/dictionary.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // fetchGames()
+    
+    
+
+
 const searchFormButton = document.getElementById("search-for");
 const addFormButton = document.getElementById("add");
 const viewButton = document.getElementById("games-list");
@@ -12,3 +18,5 @@ const basicUrl= "http://localhost:3000"
 const gameList = document.getElementById('game-list');
 const gamesCard = document.getElementById('game-card');
 const gameCard = document.getElementById('game-card');
+
+})
