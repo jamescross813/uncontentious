c26c227ce0ad1ebc4e6aebc5a617b5cd831a3be6
jamescross <crossjames813@gmail.com>
2021-09-04 19:54:24 -0500

    constants moved to dictionary

---
+++ frontend/src/dictionary.js
@@ -0,0 +1,17 @@
+const searchFormButton = document.getElementById("search-for");
+const addFormButton = document.getElementById("add");
+const searchForm = document.getElementById("search-games");
+const addForm = document.getElementById("add-game-form");
+const viewButton = document.getElementById("games-list");
+const searchButton = document.getElementById("submit-search")
+const addSubmit = document.getElementById("add-submit")
+const titleOne = document.querySelector('h3')
+const randomize = document.getElementById("random-game")
+
+const basicUrl= "http://localhost:3000"
+const gameList = document.getElementById('game-list');
+const searchForm = document.getElementById("search-games");
+const addForm = document.getElementById("add-game-form");
+const gamesCard = document.getElementById('game-card');
+
+const gameCard = document.getElementById('game-card');
