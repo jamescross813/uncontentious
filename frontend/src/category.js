4a1d8da953f65aede8377d162512ca3c048da5d7
jamescross <crossjames813@gmail.com>
2021-09-05 20:18:23 -0500

    renderCat

--- frontend/src/category.js
+++ frontend/src/category.js
@@ -1,13 +1,21 @@
 class Category{
     static all = []
-    constructor(name, gameId){
-        this.name = name;
-        this.gameId = gameId
-
+    constructor({category_name}){
+        this.name = category_name;
+        // this.gameId = gameId
         Category.all.push(this)
     }
 
     static renderCategories(data){
-        console.log(data)
+        return data.map(r => {
+            const newCat = new Category(r)
+            newCat.renderName(r)
+        })
+    }
+
+    renderName(){
+        let catName = document.createElement('h2')
+            catName.innerHTML = this.name
+            gameCard.append(catName)
     }
 }
