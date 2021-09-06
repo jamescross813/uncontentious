46e984cbbe5c068c459a4bc9a86bbbd7bb028b1e
jamescross <crossjames813@gmail.com>
2021-09-05 20:54:51 -0500

    serializer added

--- backend/app/serializers/game_serializer_serializer.rb
+++ backend/app/serializers/game_serializer_serializer.rb
@@ -1,4 +1,5 @@
 class GameSerializerSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :game_title, :min_players, :max_players, :description, :play_style, :game_type, :max_time
+  belongs_to :game_categories
 end
