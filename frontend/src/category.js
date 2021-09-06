b06d41009c56f6af2e36a2376695279afc4724d0
jamescross <crossjames813@gmail.com>
2021-09-06 18:54:45 -0500

    commented out category class

--- frontend/src/category.js
+++ frontend/src/category.js
@@ -1,37 +1,37 @@
-class Category{
-    static all = []
-    constructor({category_name, id}){
-        this.name = category_name;
-        this.id = id;
-        Category.all.push(this)
-    }
+// class Category{
+//     static all = []
+//     constructor({category_name, id}){
+//         this.name = category_name;
+//         this.id = id;
+//         Category.all.push(this)
+//     }
 
-    static renderCategories(data){
-        return data.map(r => {
-            const newCat = new Category(r)
-            newCat.renderName(r)
-        })
-    }
+//     static renderCategories(data){
+//         return data.map(r => {
+//             const newCat = new Category(r)
+//             newCat.renderName(r)
+//         })
+//     }
     
-    static renderCategoriesForForm(data){
+//     static renderCategoriesForForm(data){
        
-        return data.map(r => {
-            const newFormCat = new Category(r)
-            newFormCat.renderNewForm(r)
-        })
-    }
+//         return data.map(r => {
+//             const newFormCat = new Category(r)
+//             newFormCat.renderNewForm(r)
+//         })
+//     }
     
-    renderName(){
-        let catName = document.createElement('h3')
-            catName.innerHTML = this.name
-            catCard.append(catName)
-    }
+//     renderName(){
+//         let catName = document.createElement('h3')
+//             catName.innerHTML = this.name
+//             catCard.append(catName)
+//     }
 
-    renderNewForm(){
-       let `catOptions${this.id}` = document.createElement('h3') 
-        catOptions.innerHTML = `
-        <input type="checkbox" class="cat-input" id="category-input-${this.id}" name="category" placeholder="Category" class="input-text"
-                    value="${this.name}">${this.name}`
-        catSection.append(catOptions)
-    }
-}
+//     renderNewForm(){
+//        let `catOptions${this.id}` = document.createElement('h3') 
+//         catOptions.innerHTML = `
+//         <input type="checkbox" class="cat-input" id="category-input-${this.id}" name="category" placeholder="Category" class="input-text"
+//                     value="${this.name}">${this.name}`
+//         catSection.append(catOptions)
+//     }
+// }
