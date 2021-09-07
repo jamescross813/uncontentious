6e21b45519480aab4b5b5e36a0db7f2ce866c59d
jamescross <crossjames813@gmail.com>
2021-09-07 08:21:14 -0500

    patch ajax added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -47,5 +47,37 @@ class Api{
         .then(resp => resp.json())
         .then(data => Game.renderGame(data))
       }
+
+      editGamePost(){
+       
+        let inputValue = document.getElementsByClassName("input-text");
+        let titleInput = document.getElementById('title-input');
+    
+        let gameFormData = {
+            game_categories: `${inputValue[6].value}, ${inputValue[7].value}`,
+            description: inputValue[8].value,
+            min_players: inputValue[1].value,
+            max_players: inputValue[2].value,
+            max_time: inputValue[5].value,
+            play_style: inputValue[4].value,
+            game_type: inputValue[3].value,
+            game_title: titleInput.value
+            
+            };
+            
+        let configObj = {
+            method: "PATCH",
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(gameFormData)
+        };
+    
+        fetch(`${basicUrl}/games`, configObj)
+        .then(resp => resp.json())
+        .then(data => Game.renderGame(data))
+      }
+
       
 }
