09389b342d902a95089ff3b2b24c8dcba2aa7959
jamescross <crossjames813@gmail.com>
2021-09-06 19:03:22 -0500

    removed excess code

--- backend/app/models/game.rb
+++ backend/app/models/game.rb
@@ -1,8 +1,6 @@
 class Game < ApplicationRecord
   has_many :user_games
   has_many :notes
-  has_many :game_categories
-  has_many :games, through: :game_categories
-
+  
   
 end
--- backend/app/models/game_category.rb
+++
@@ -1,4 +0,0 @@
-class GameCategory < ApplicationRecord
-  belongs_to :category
-  belongs_to :game
-end
