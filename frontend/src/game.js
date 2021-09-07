f4cfa1dcc55109f424da2f46683957952d0b6cc6
jamescross <crossjames813@gmail.com>
2021-09-07 08:32:04 -0500

    formatting

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -69,11 +69,12 @@ static all = []
         this.categories.map(c=>
             gameCat.innerHTML = `Category: ${this.categories}`)
         
-       let editButton = document.createElement('button')
-       editButton.innerHTML = `<type="button" id ="edit-button">Edit Game`
+        let editButton = document.createElement('button')
+        editButton.innerHTML = `<type="button" id ="edit-button">Edit Game`
 
-       let deleteButton = document.createElement('button')
-       deleteButton.innerHTML = `<type="button" id ="delete-button">Delete Game`
+        let deleteButton = document.createElement('button')
+        deleteButton.innerHTML = `<type="button" id ="delete-button">Delete Game`
+
         gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType, gameCat, editButton, deleteButton)
     }
 
