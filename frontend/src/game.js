ad31e1ae2fc67f8365ef7bb8e086492a5bb5f905
jamescross <crossjames813@gmail.com>
2021-09-07 09:18:26 -0500

    edit button not working, attempt to fix unsuc

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -69,8 +69,13 @@ static all = []
         this.categories.map(c=>
             gameCat.innerHTML = `Category: ${this.categories}`)
         
+    //    let editButton = document.createElement('button')
+    //    editButton.innerHTML = `<type="button" id ="edit-button">Edit Game`
 
-        gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType, gameCat, editButton, deleteButton)
+    //    let deleteButton = document.createElement('button')
+    //    deleteButton.innerHTML = `<type="button" id ="delete-button">Delete Game`
+       
+        gameCard.append(gameTitle, gameDescription, gamePlayers, gamePlayStyle, gameTime, gameType, gameCat)
     }
 
     // newGamePost(){
