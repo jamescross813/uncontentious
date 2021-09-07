f534a84849b13e1d8b46b8129aba3c0f2f1007e7
jamescross <crossjames813@gmail.com>
2021-09-07 17:44:50 -0500

    randomize button const

--- frontend/src/dictionary.js
+++ frontend/src/dictionary.js
@@ -1,10 +1,10 @@
 const searchFormButton = document.getElementById("search-for");
 const addFormButton = document.getElementById("add");
 const viewButton = document.getElementById("games-list");
-const catButton = document.getElementById("categories")
 const searchButton = document.getElementById("submit-search")
 const addSubmit = document.getElementById("add-submit")
 const editButton = document.getElementById("edit-game")
+const randomButton = document.getElementById("randomize")
 
 const searchForm = document.getElementById("search-games");
 const addForm = document.getElementById("add-game-form");
