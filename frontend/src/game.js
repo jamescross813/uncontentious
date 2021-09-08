39b6ba4833e517a4510cd3eb8eb594b8a5fe5a83
jamescross <crossjames813@gmail.com>
2021-09-07 22:16:35 -0500

    added edit to input class name

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -78,23 +78,23 @@ class Game{
             let editForm = document.createElement('form')
             editForm.innerHTML = `
             <label>Title:</label>
-            <input type="text" id="title-input" name="game-title" class="input-text" value="${data.game_title}"><br>
+            <input type="text" id="title-input" name="game-title" class="input-text-edit" value="${data.game_title}"><br>
             <label>Minimum Players:</label>
-            <input type="text" id="player-range-min-input" name="player-range-min" class="input-text" value="${data.min_players}"><br>
+            <input type="text" id="player-range-min-input" name="player-range-min" class="input-text-edit" value="${data.min_players}"><br>
             <label>Maximum Players:</label>        
-            <input type="text" id="player-range-max-input" name="player-range-max" class="input-text" value="${data.max_players}"><br>
+            <input type="text" id="player-range-max-input" name="player-range-max" class="input-text-edit" value="${data.max_players}"><br>
             <label>Game Type:</label>        
-            <input type="text" id="game-type-input" name="game-type" class="input-text" value="${data.game_type}"><br>
+            <input type="text" id="game-type-input" name="game-type" class="input-text-edit" value="${data.game_type}"><br>
             <label>Game Style:</label>        
-            <input type="text" id="play-style-input" name="play-style" class="input-text" value="${data.play_style}"><br>
+            <input type="text" id="play-style-input" name="play-style" class="input-text-edit" value="${data.play_style}"><br>
             <label>Max Play Time:</label>        
-            <input type="text" id="play-time-input" name="play-time" value="${data.max_time}"><br>
+            <input type="text" id="play-time-input" name="play-time" class="input-text-edit" value="${data.max_time}"><br>
             <label>Category</label>        
-            <input type="text" id="category-one" name="game-category-one" value="${data.game_categories}"><br>
+            <input type="text" id="category-one" class="input-text-edit" name="game-category-one" value="${data.game_categories}"><br>
             <label>Additional Category:</label>        
-            <input type="text" id="category-two" name="game-category-two" value=""><br>
+            <input type="text" id="category-two" class="input-text-edit" name="game-category-two" value=""><br>
             <label>Description:</label>        
-            <input type="text" id="description-input" name="description" class="input-text" value="${data.description}"></textarea><br>
+            <input type="text" id="description-input" name="description" class="input-text-edit" value="${data.description}"></textarea><br>
                     <button type="button" id="edit-submit">Edit Game</button>`
             gameCard.append(editForm)
 
