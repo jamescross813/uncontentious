bf3401ee7a23d9ba3bc55b128c59bfdcb2e4d89a
jamescross <crossjames813@gmail.com>
2021-09-07 22:19:26 -0500

    edit fetch and subsequent render working

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -56,20 +56,20 @@ class Api{
 
       editGamePatch(){
        event.preventDefault()
-        let inputValue = document.getElementsByClassName("input-text");
-        let titleInput = document.getElementById('title-input');
+        let editInputValue = document.getElementsByClassName("input-text-edit");
+        // let titleInput = document.getElementById('title-input');
     
         let gameFormData = {
-            game_categories: `${inputValue[6].value}, ${inputValue[7].value}`,
-            description: inputValue[8].value,
-            min_players: inputValue[1].value,
-            max_players: inputValue[2].value,
-            max_time: inputValue[5].value,
-            play_style: inputValue[4].value,
-            game_type: inputValue[3].value,
-            game_title: titleInput.value
+            game_categories: `${editInputValue[6].value}, ${editInputValue[7].value}`,
+            description: editInputValue[8].value,
+            min_players: editInputValue[1].value,
+            max_players: editInputValue[2].value,
+            max_time: editInputValue[5].value,
+            play_style: editInputValue[4].value,
+            game_type: editInputValue[3].value,
+            game_title: editInputValue[0].value
             };
-            console.log(inputValue)
+            console.log(editInputValue[0].value)
             
         let configObj = {
             method: "PATCH",
@@ -82,7 +82,7 @@ class Api{
     
         fetch(`${this.basicUrl}`, configObj)
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        .then(data => Game.renderGame(data))
       }
 
       
