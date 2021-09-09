fca4c359f3ab8f0819e9f5be7efbdb5919ef1163
jamescross <crossjames813@gmail.com>
2021-09-08 20:27:43 -0500

    tidy up labeling functions in event listeners

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", ()=>{
     const gameApi = new Api(`${basicUrl}/games`)
 
-    addFormButton.addEventListener('click', function () {
+    addFormButton.addEventListener('click', ()=>{
         if(!gamesList.hidden){
             gamesList.hidden = !gamesList.hidden
         }
@@ -18,19 +18,19 @@ document.addEventListener("DOMContentLoaded", function() {
         gameApi.fetchGames()   
     })  
 
-    addSubmit.addEventListener('click', function(){   
+    addSubmit.addEventListener('click', ()=>{   
        gameApi.newGamePost()
        gameSection.hidden = !gameSection.hidden
        addForm.reset() 
     }) 
 
-    editButton.addEventListener('click', () =>{
+    editButton.addEventListener('click', ()=>{
        let game_id = editButton.name
        let currentGameApi = new Api(`${basicUrl}/games/${game_id}`)
         currentGameApi.fetchEditGame()
     })
 
-    randomButton.addEventListener('click', () =>{
+    randomButton.addEventListener('click', ()=>{
 
         if(!gamesList.hidden || !addForm.hidden){
             gamesList.hidden = true
