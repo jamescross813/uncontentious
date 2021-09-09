211774cf56d1941741f76e23ec3914cba8897305
jamescross <crossjames813@gmail.com>
2021-09-08 21:17:38 -0500

    removed unecessary code:

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-    has_many :user_games
     has_many :notes
 
-    has_secure_password
-
-    validates :username, uniqueness: true, presence: true
-    validates :password_digest, presence:true
+    
 end
