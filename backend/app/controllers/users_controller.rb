0cadc9284d54e685a6de3a7ab310f7b9e358b707
jamescross <crossjames813@gmail.com>
2021-09-08 20:52:54 -0500

    issue with params sorted

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password_digest)
     end
 end
