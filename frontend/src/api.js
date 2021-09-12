939c3e4a5dd910cb2b9d309b65afbc06da78d99b
jamescross <crossjames813@gmail.com>
2021-09-11 22:51:01 -0500

    formatting code

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -17,7 +17,6 @@ class Api{
         if (data.hasOwnProperty('message')){
           randomizer()
         }else{
-          // console.log(data)
           Game.renderGame(data)
         }
       })
@@ -30,121 +29,115 @@ class Api{
   };
 
   fetchNote() {
-    // gamesList.innerHTML = ""
-    // console.log(this.basicUrl)
       fetch(this.basicUrl)
       .then(resp =>resp.json())
       .then(data =>Note.renderNote(data))
     };
 
-      newGamePost(){
+  newGamePost(){
        
-        let inputValue = document.getElementsByClassName("input-text");
-        let titleInput = document.getElementById('title-input');
+      let inputValue = document.getElementsByClassName("input-text");
+      let titleInput = document.getElementById('title-input');
     
-        let gameFormData = {
-            game_categories: `${inputValue[6].value}, ${inputValue[7].value}`,
-            description: inputValue[8].value,
-            min_players: inputValue[1].value,
-            max_players: inputValue[2].value,
-            max_time: inputValue[5].value,
-            play_style: inputValue[4].value,
-            game_type: inputValue[3].value,
-            game_title: titleInput.value
-            };
+      let gameFormData = {
+          game_categories: `${inputValue[6].value}, ${inputValue[7].value}`,
+          description: inputValue[8].value,
+          min_players: inputValue[1].value,
+          max_players: inputValue[2].value,
+          max_time: inputValue[5].value,
+          play_style: inputValue[4].value,
+          game_type: inputValue[3].value,
+          game_title: titleInput.value
+          };
             
-        let configObj = {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(gameFormData)
-        };
+      let configObj = {
+          method: "POST",
+          headers:{
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+          },
+          body: JSON.stringify(gameFormData)
+       };
     
-        fetch(`${basicUrl}/games`, configObj)
-        .then(resp => resp.json())
-        .then(data => Game.renderGame(data))
-      }
+      fetch(`${basicUrl}/games`, configObj)
+      .then(resp => resp.json())
+      .then(data => Game.renderGame(data))
+  }
 
-      editGamePatch(){
-       event.preventDefault()
-        let editInputValue = document.getElementsByClassName("input-text-edit");
+  editGamePatch(){
+      event.preventDefault()
+      let editInputValue = document.getElementsByClassName("input-text-edit");
     
-        let gameFormData = {
-            game_categories: `${editInputValue[6].value}, ${editInputValue[7].value}`,
-            description: editInputValue[8].value,
-            min_players: editInputValue[1].value,
-            max_players: editInputValue[2].value,
-            max_time: editInputValue[5].value,
-            play_style: editInputValue[4].value,
-            game_type: editInputValue[3].value,
-            game_title: editInputValue[0].value
-            };
-            console.log(editInputValue[0].value)
+      let gameFormData = {
+          game_categories: `${editInputValue[6].value}, ${editInputValue[7].value}`,
+          description: editInputValue[8].value,
+          min_players: editInputValue[1].value,
+          max_players: editInputValue[2].value,
+          max_time: editInputValue[5].value,
+          play_style: editInputValue[4].value,
+          game_type: editInputValue[3].value,
+          game_title: editInputValue[0].value
+         };
             
-        let configObj = {
-            method: "PATCH",
-            headers:{
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(gameFormData)
-        };
+      let configObj = {
+          method: "PATCH",
+          headers:{
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+          },
+          body: JSON.stringify(gameFormData)
+      };
     
-        fetch(`${this.basicUrl}`, configObj)
-        .then(resp => resp.json())
-        .then(data => Game.renderGame(data))
-      }
+      fetch(`${this.basicUrl}`, configObj)
+      .then(resp => resp.json())
+      .then(data => Game.renderGame(data))
+  }
 
-      newUserPost(){
+  newUserPost(){
        
-        let usernameInput = document.getElementById("username-input");
+      let usernameInput = document.getElementById("username-input");
             
-        let userFormData = {
-           username: usernameInput.value
-            };
+      let userFormData = {
+        username: usernameInput.value
+      };
             
-        let configObj = {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(userFormData)
-        };
+      let configObj = {
+          method: "POST",
+          headers:{
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+          },
+          body: JSON.stringify(userFormData)
+      };
     
-        fetch(`${this.basicUrl}`, configObj)
-        .then(resp => resp.json())
-        .then(data => User.renderIntro(data))
-      }
+      fetch(`${this.basicUrl}`, configObj)
+      .then(resp => resp.json())
+      .then(data => User.renderIntro(data))
+   }
 
-      newNotePost(){
+  newNotePost(){
        
-        let usernameInput = document.getElementById("username-input");
-        let contentInput = document.getElementById("note-content-input");
-        let gameId = gameCard.id  
+      let usernameInput = document.getElementById("username-input");
+      let contentInput = document.getElementById("note-content-input");
+      let gameId = gameCard.id  
       
-        let noteFormData = {
-            username: usernameInput.value,
-            game_id: gameId,
-            content: contentInput.value
-            };
+      let noteFormData = {
+          username: usernameInput.value,
+          game_id: gameId,
+          content: contentInput.value
+          };
             
-        let configObj = {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(noteFormData)
-        };
+      let configObj = {
+          method: "POST",
+          headers:{
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+          },
+          body: JSON.stringify(noteFormData)
+      };
     
-        fetch(`${this.basicUrl}`, configObj)
-        .then(resp => resp.json())
-        .then(data => Note.renderNote(data))
-      }
-
-
-      
+      fetch(`${this.basicUrl}`, configObj)
+      .then(resp => resp.json())
+      .then(data => Note.renderNote(data))
+  }
 }
